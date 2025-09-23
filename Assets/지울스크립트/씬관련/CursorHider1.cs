01b8815976ef8057c262f6321c7dc2f926847a0a
MSI\zmttt <24cr0141@jec.ac.jp>
2025-09-23 16:25:09 +0900

    ステージ修正 (途中)

---
+++ Assets/지울스크립트/씬관련/CursorHider1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHider1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 씬이 시작될 때 마우스 커서를 보이지 않게 합니다.
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
